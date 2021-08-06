e62b65192ef4b83aaf798401cb3214643ef70bb9
PetraE348 <79121088+PetraE348@users.noreply.github.com>
2021-08-06 13:18:52 +1200

    Add files via upload

--- interface.py
+++ interface.py
@@ -90,20 +90,21 @@ class interface:
 
         def questions1(self):
                 self.topic1_frame.grid_forget()
-                self.questions1_frame = Frame(root, width="200", height="200")
-                self.questions1_frame.grid(row=0, column=0, padx=10, pady=5)
-                self.title = Label(self.questions1_frame, text="Level One 'topic' ").grid(column=3, row=0, pady=5)
+                self.questions1_frame = Frame(root, width="600", height="600")
+                self.questions1_frame.grid(row=0, column=0)
+                self.title = Label(self.questions1_frame,font=("Arial 18 bold underline"), text="MATHS HELPER",bg="skyblue").grid(column=2, row=0, padx=150)
+                self.title2 = Label(self.questions1_frame, text="Level One Addition ", font=50).grid(column=2, row=1)
                 self.question_win()
-                self.back = Button(self.questions1_frame, text="Back", command=self.backq1).grid(column=3, row=4)
-                self.finish = Button(self.questions1_frame, text="Finished", command=self.q1gridforget).grid(column=4, row=4)
+                self.back = Button(self.questions1_frame, text="Back", command=self.backq1).grid(column=1, row=4)
+                self.finish = Button(self.questions1_frame, text="Finished", command=self.q1gridforget).grid(column=3, row=0)
                 self.solving = Entry(self.questions1_frame)
-                self.solving.grid(column=1, row=3)
+                self.solving.grid(column=3, row=3)
 
                 self.check = Button(self.questions1_frame, text="Submit", command= lambda: self.checkb(self.solving))
-                self.check.grid(column=1, row=4)
+                self.check.grid(column=3, row=4)
 
                 self.nextb = Button(self.questions1_frame, text="Next", command=self.question_win)
-                self.nextb.grid(column=1,row=5)
+                self.nextb.grid(column=3,row=2)
                 
         def checkb(self,var1):
                 if var1.get() == str(self.correct_answer()):
@@ -118,8 +119,8 @@ class interface:
                 self.num1update = random.randrange(1,20)
                 self.num2update = random.randrange(1,20)
                 
-                question = Label(self.questions1_frame, text=f"{self.num1update} + {self.num2update}")
-                question.grid(column=1, row=1)
+                question = Label(self.questions1_frame, text=f"{self.num1update} + {self.num2update} = ")
+                question.grid(column=2, row=2)
 
         def correct_answer(self):                
                 return self.num1update + self.num2update
