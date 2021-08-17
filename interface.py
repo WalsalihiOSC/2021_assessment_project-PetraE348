f8ec6fc82e40ec7434d68e6a691cba414c7c729d
PetraE348 <79121088+PetraE348@users.noreply.github.com>
2021-08-17 15:08:46 +1200

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,225 +1,162 @@
 from tkinter import *
 root = Tk()
-root.title("Maths Helper")
-root.config(bg="skyblue")
-import random
 
-#Define class
 class interface:
 #Class for welcome window
         def __init__(self):
-                #Creating the menu frame
-                #Creating the menu frame
-                self.menu = Frame(root, width="600", height="600",bg="skyblue")
-                self.menu.grid(row=0, column=0)
-                #Title 'Maths Helper'
-                Label(self.menu, text="         ",bg="skyblue").grid(column=1, row=1)
-                Label(self.menu, text="         ",bg="skyblue").grid(column=2, row=0, pady= 20)
-                Label(self.menu, text="         ",bg="skyblue").grid(column=2, row=2, pady= 20)
-                Label(self.menu, font=("Arial 18 bold underline"), text="MATHS HELPER",bg="skyblue",).grid(column=2, row=1)
-                #Label for player name
-                Label(self.menu, text="Enter your \nname:", font=100, fg='#9c57d5',bg="skyblue").grid(column=1, row=3, padx=30)
-                #Entry for player name
-                self.p_n = Entry(self.menu, text="         ", font=50)
-                self.p_n.grid(column=2, row=3)
-                #Lable for level selection 
-                Label(self.menu, text="Select a \nlevel:", font=50, fg='black',bg="skyblue").grid(column=1, row=4)
-                Button(self.menu, text="done", command=self.done).grid(column=3, row=5)
-                #Dropdown menu for levels
-                self.tkvar = StringVar(root)
+            self.name_win = Frame(root, width="200", height="200")
+            self.name_win.grid()
+
+            #Title 'Student Details'
+            self.title = Label(self.name_win, font=("Arial 18 bold underline"), text="MATHS HELPER").grid(column=3, row=0, pady=5)
+            self.namel = Label(self.name_win, text="Enter your name: ", bg='#9c57d5').grid(column=3, row=1)
+            self.name = Entry(self.name_win, text="            ")
+            self.name.grid(column=4, row=1)
+            self.submitb = Button(self.name_win, text="Submit", bg='#99dd1c', command=self.submit).grid(column=5, row=4)
+
+        def submit(self):
+            self.name = (self.name.get().title())
+            self.name_win.grid_forget()
+            self.level_win()
+
+        def level_win(self):
+            self.level_frame = Frame(root, width="200", height="200")
+            self.level_frame.grid()
+            self.title = Label(self.level_frame, text="MATHS HELPER").grid(column=3, row=0, pady=5)
+            self.label = Label(self.level_frame, text="Select a level: ", fg='black').grid(column=3, row=1)
+            self.level1 = Button(self.level_frame, text="Level 1", bg="#FF00FF", command=self.topic1_win ).grid(column=3, row=3)
+            self.level2 = Button(self.level_frame, text="Level 2", bg="#99dd1c", command=self.topic2_win ).grid(column=3, row=4)
+            self.level3 = Button(self.level_frame, text="Level 3", bg="#25b1e9", command=self.topic3_win ).grid(column=3, row=5)
+            self.backlevel = Button(self.level_frame, text="Back", command=self.backlevel).grid(column=3, row=6)
         
-        # Set options
-                self.choices = ['One', 'Two', 'Three']
-                self.tkvar.set('Select Level')
-                self.dropdown = OptionMenu(self.menu, self.tkvar, *self.choices)
+        def backlevel(self):
+            self.level_frame.grid_forget()
+            self.__init__()
         
-        # Create Label
-                self.leveldropdown = self.dropdown.grid(column=2,row=4)
+        def topic1_win(self):
+            self.level_frame.grid_forget()
+            self.topic1_frame = Frame(root, width="200", height="200")
+            self.topic1_frame.grid()
+            self.title = Label(self.topic1_frame, text="MATHS HELPER").grid(column=3, row=0, pady=5)
+            self.label= Label(self.topic1_frame, text="Select a topic: ", fg='black').grid(column=3, row=1)
+            self.addition1= Button(self.topic1_frame, text="Addition", bg="#FF00FF", command=self.questions1 ).grid(column=3, row=3)
+            self.subtraction1 = Button(self.topic1_frame, text="Subtraction", bg="#99dd1c", command=self.questions1 ).grid(column=3, row=4)
+            self.multiplication1 = Button(self.topic1_frame, text="Multiplication", bg="#25b1e9", command=self.questions1 ).grid(column=3, row=5)
+            self.division1 = Button(self.topic1_frame, text="Division", bg="#25b1e9", command=self.questions1 ).grid(column=3, row=6)
+            self.back = Button(self.topic1_frame, text="Back", command=self.back1).grid(column=3, row=8)
         
-                def change_dropdown(*args):
-                        self.method = (self.tkvar.get())
-                self.tkvar.trace('w', change_dropdown)
-                self.submit = Button(self.menu, text="  Submit ", font=50, bg='#99dd1c', command=self.submit_check).grid(column= 3, row=4 ,padx=20)
+        def back1(self):
+            self.topic1_frame.grid_forget()
+            self.level_win()
+
+        def topic2_win(self):
+            self.level_frame.grid_forget()
+            self.topic2_frame = Frame(root, width="200", height="200")
+            self.topic2_frame.grid()
+            self.title = Label(self.topic2_frame, text="MATHS HELPER").grid(column=3, row=0, pady=5)
+            self.label= Label(self.topic2_frame, text="Select a topic: ", fg='black').grid(column=3, row=1)
+            self.addition2= Button(self.topic2_frame, text="Addition", bg="#FF00FF", command=self.questions2  ).grid(column=3, row=3)
+            self.subtraction2 = Button(self.topic2_frame, text="Subtraction", bg="#99dd1c", command=self.questions2  ).grid(column=3, row=4)
+            self.multiplication2 = Button(self.topic2_frame, text="Multiplication", bg="#25b1e9", command=self.questions2  ).grid(column=3, row=5)
+            self.division2 = Button(self.topic2_frame, text="Division", bg="#25b1e9", command=self.questions2 ).grid(column=3, row=6)
+            self.back = Button(self.topic2_frame, text="Back", command=self.back2).grid(column=3, row=8)
+
+        def back2(self):
+            self.topic2_frame.grid_forget()
+            self.level_win()
+
+        def topic3_win(self):
+            self.level_frame.grid_forget()
+            self.topic3_frame = Frame(root, width="200", height="200")
+            self.topic3_frame.grid()
+            self.title = Label(self.topic3_frame, text="MATHS HELPER").grid(column=3, row=0, pady=5)
+            self.label= Label(self.topic3_frame, text="Select a topic: ", fg='black').grid(column=3, row=1)
+            self.addition3= Button(self.topic3_frame, text="Addition", bg="#FF00FF", command=self.questions3 ).grid(column=3, row=3)
+            self.subtraction3 = Button(self.topic3_frame, text="Subtraction", bg="#99dd1c", command=self.questions3 ).grid(column=3, row=4)
+            self.multiplication3 = Button(self.topic3_frame, text="Multiplication", bg="#25b1e9", command=self.questions3 ).grid(column=3, row=5)
+            self.division3 = Button(self.topic3_frame, text="Division", bg="#25b1e9", command=self.questions3 ).grid(column=3, row=6)
+            self.back = Button(self.topic3_frame, text="Back", command=self.back3).grid(column=3, row=8)
         
-        #defining function (submit) which error checks name
-        def submit_check(self):
-                self.count = 1
-                #captialises the first letter of players name
-                self.player_name = self.p_n.get().capitalize()
-                self.level = self.tkvar.get()
-                #if player name entry box is empty then sent error otherwise continue to questions frame & destroy current frame        
-                if len(self.player_name)==0:
-                        self.notvalid = True
-                        Label(self.menu, text="Name Required*", fg='red').grid(column=3, row=1, sticky='e')     
-                
-                elif self.level == "Select Level":
-                        self.notvalid = True
-                        Label(self.menu, text="Level Required*", fg='red').grid(column=3, row=2, sticky='e') 
-                else:
-                        self.notvalid = False
-                        self.questions_win1()
+        def back3(self):
+            self.topic3_frame.grid_forget()
+            self.level_win()
 
-#############################################
-############### LEVEL WINDOWS ###############
-#############################################
+        def questions1(self):
+            self.topic1_frame.grid_forget()
+            self.questions1_frame = Frame(root, width="200", height="200")
+            self.questions1_frame.grid()
+            self.title = Label(self.questions1_frame, text="Level One 'topic' ").grid(column=3, row=0, pady=5)
+            self.back = Button(self.questions1_frame, text="Back", command=self.backq1).grid(column=3, row=4)
+            self.finish = Button(self.questions1_frame, text="Finished", command=self.q1gridforget).grid(column=4, row=4)
 
-#####################
-##### LEVEL ONE #####
-#####################
-        #defines question win function
-        def questions_win1(self):
-                self.count = 1
-                self.menu.grid_remove()
-                self.score = 0
-                #creates the question1 frame
-                self.questions1_frame = Frame(root, width="600", height="600",bg="skyblue")
-                self.questions1_frame.grid(row=0, column=0)
-                Label(self.questions1_frame, text="         ",bg="skyblue").grid(column=1, row=0, padx=40, pady=20)
-                Label(self.questions1_frame, text="         ",bg="skyblue").grid(column=2, row=0, pady= 20)
-                Label(self.questions1_frame, text="         ",bg="skyblue").grid(column=2, row=2, pady= 20)
-        
-                #title "Maths Helper"
-                Label(self.questions1_frame,font=("Arial 18 bold underline"), text=f"MATHS HELPER- Level {self.level}",bg="skyblue").grid(column=2, row=1)
+        def backq1(self):
+            self.questions1_frame.grid_forget()
+            self.topic1_win()
+            
+#impliment loop?
+        def questions2(self):
+            self.topic2_frame.grid_forget()
+            self.questions2_frame = Frame(root, width="200", height="200")
+            self.questions2_frame.grid()
+            self.title = Label(self.questions2_frame, text="Level Two 'topic' ").grid(column=3, row=0, pady=5)
+            self.back = Button(self.questions2_frame, text="Back", command=self.backq2).grid(column=3, row=4)
+            self.finish = Button(self.questions2_frame, text="Finished", command=self.q2gridforget).grid(column=4, row=4)
 
-                #calls function for questions
-                self.question_reload1()
-                #entry for answer
-                self.solving = Entry(self.questions1_frame, text="      " ,font=50)
-                self.solving.grid(column=2, row=3)
-                
-                self.submit_spawn()
-                #submit button
-                self.check = Button(self.questions1_frame, text="Submit", font=50, bg='#99dd1c', command= lambda: self.checkb1(self.solving))
-                self.check.grid(column=3, row=4)
+        def backq2(self):
+            self.questions2_frame.grid_forget()
+            self.topic2_win()
+            
 
-        def submit_spawn(self):
-                Label(self.questions1_frame, text="         ",bg="skyblue").grid(column=1, row=3, padx=40, pady=20)
-                Label(self.questions1_frame, text="         ",bg="skyblue").grid(column=1, row=4, padx=40, pady=20)
-                Label(self.questions1_frame, text="         ",bg="skyblue").grid(column=1, row=5, padx=40, pady=20)
-                
-                #next button for new question
+#impliment loop?
+        def questions3(self):
+            self.topic3_frame.grid_forget()
+            self.questions3_frame = Frame(root, width="200", height="200")
+            self.questions3_frame.grid()
+            self.title = Label(self.questions3_frame, text="Level Three 'topic' ").grid(column=3, row=0, pady=5)
+            self.back = Button(self.questions3_frame, text="Back", command=self.backq3).grid(column=3, row=4)
+            self.finish = Button(self.questions3_frame, text="Finished", command=self.q3gridforget).grid(column=4, row=4)
 
-        def checkb1(self,var1):
-
-                
-                if var1.get() == str(self.correct_answer()):
-                        self.score +=1
-                        self.feed_back = Label(self.questions1_frame, text="Correct!", fg="green")
-                        self.feed_back.grid(column=3, row=3)
-                        self.solving.delete(0,'end')
-                        self.check.grid_remove()
-                        self.questions1_nextb = Button(self.questions1_frame, text=" Next ",font=50, bg='#99dd1c', command=self.nextbtn)
-                        self.questions1_nextb.grid(column=3,row=4)
-                        self.count += 1
-
-                else:
-                        self.feed_back = Label(self.questions1_frame, text="Wrong!", fg="red")
-                        self.feed_back.grid(column=3, row=3)
-                        self.solving.delete(0,'end')
-                        self.check.grid_remove()
-                        self.questions1_nextb = Button(self.questions1_frame, text=" Next ",font=50, bg='#99dd1c', command=self.nextbtn)
-                        self.questions1_nextb.grid(column=3,row=4)
-                        self.count += 1
-
-                 # button == 10 times 
-                if self.count == 11:
-                        self.check.config(state=DISABLED)
-                        self.check.unbind("<Button-1>")
-                        self.time = 11
-                        self.count=0
-                        def countdown():
-                                if self.time >= 0:
-                                        # End of Q
-                                        self.feed_back.grid_remove()
-                                        Button(self.questions1_frame, text=" End game  ",font=50, bg='#99dd1c', command=self.leaderboard).grid(column=3,row=4)
-                                        # next Frame button
-                                        self.time -= 1
-                                else:
-                                        global count
-                                        self.check.config(state=NORMAL)
-                        countdown()
-                        
-                        
-        def nextbtn(self):
-                self.questions1_nextb.grid_remove()
-                self.feed_back.grid_remove()
-                self.question_reload1()
-                self.check = Button(self.questions1_frame, text="Submit", font=50, bg='#99dd1c', command= lambda: self.checkb1(self.solving))
-                self.check.grid(column=3, row=4)
-                
-        def question_reload1(self):
-
-                if self.level == "One":
-                        self.num1update = random.randrange(1,20)
-                        self.num2update = random.randrange(1,20)
-
-                elif self.level == "Two":
-                        self.num2update = random.randrange(1,50)
-                        self.num2update = random.randrange(1,50)
-                
-                else:
-                        self.num1update = random.randrange(1,100)
-                        self.num2update = random.randrange(1,100)
-                        
-                
-                self.question = Label(self.questions1_frame,font=50, text=f" {self.num1update} + {self.num2update} =  ",bg="skyblue").grid(column=2, row=2)
-                Label(self.questions1_frame, text=f" {self.score} /10", font=50, bg="skyblue").grid(column=3, row=5)
-                Label(self.questions1_frame, text=f"Q:{self.count}", font=50, bg="skyblue").grid(column=1, row=1)
-################
-##### ALL ######
-################            
-
-        def correct_answer(self):                
-                return self.num1update + self.num2update
-                
-                
+        def backq3(self):
+            self.questions3_frame.grid_forget()
+            self.topic3_win()
+#impliment loop?
         def q1gridforget(self):
-                self.questions1_frame.destroy()
-                self.leaderboard()
+            self.questions1_frame.grid_forget()
+            self.leaderboard()
         def q2gridforget(self):
-                self.questions2_frame.destroy()
-                self.leaderboard()
+            self.questions2_frame.grid_forget()
+            self.leaderboard()
         def q3gridforget(self):
-                self.questions3_frame.destroy()
-                self.leaderboard()
-
-
-#######################        
-##### LEADERBOARD #####     
-#######################
-        def done(self):
-                self.menu.destroy()
-                self.leaderboard()
-
+            self.questions3_frame.grid_forget()
+            self.leaderboard()
+    
+    
         def leaderboard(self):
-                #self.questions1_frame.grid_remove()
-                self.leaderboard_frame = Frame(root, width="200", height="200")
-                self.leaderboard_frame.grid(row=0, column=0, padx=10, pady=5)
-                self.restart = Button(self.leaderboard_frame, text="Restart", command=self.restartgridforget).grid(column=3, row=5, pady=5)
-                self.newplayer = Button(self.leaderboard_frame, text="New Player", command=self.npgridforget).grid(column=1, row=5, pady=5)
-                
-                self.leaderboard_frame = Frame(root, width="600", height="600",bg="skyblue")
-                self.leaderboard_frame.grid(row=0, column=0)
-                #Title 'Maths Helper'
-                Label(self.leaderboard_frame, text="         ",bg="skyblue").grid(column=1, row=1)
-                Label(self.leaderboard_frame, text="         ",bg="skyblue").grid(column=2, row=0, pady= 20)
-                Label(self.leaderboard_frame, text="         ",bg="skyblue").grid(column=2, row=2, pady= 20)
-                Label(self.leaderboard_frame, font=("Arial 18 bold underline"), text="MATHS HELPER",bg="skyblue",).grid(column=2, row=1)
-                #Label for player name
-                Label(self.leaderboard_frame, text=f"Name: {self.p_n}           Score: {self.score}/10", font=100, fg='#9c57d5',bg="skyblue").grid(column=1, row=3, padx=30)
+            self.leaderboard_frame = Frame(root, width="200", height="200")
+            self.leaderboard_frame.grid()
+            self.title = Label(self.leaderboard_frame, text="MATHS HELPER").grid(column=3, row=0, pady=5)
+            self.label = Label(self.leaderboard_frame, text="leaderboard").grid(column=3, row=1, pady=5)
+            self.restart = Button(self.leaderboard_frame, text="Restart", command=self.restartgridforget).grid(column=2, row=2, pady=5)
+            self.newplayer = Button(self.leaderboard_frame, text="New Player", command=self.npgridforget).grid(column=3, row=2, pady=5)
         
-                
         def restartgridforget(self):
-                self.leaderboard_frame.destroy()
-                self.level_win()
+            self.leaderboard_frame.grid_forget()
+            self.level_win()
 
         def npgridforget(self):
-                self.leaderboard_frame.destroy()
-                self.__init__()
+            self.leaderboard_frame.grid_forget()
+            self.__init__()
+
+
+
+
+        
+
+
 
 interface()
 
-root.title("Ormiston Computing- Maths Helper")
-root.geometry("600x400")
+root.title("Ormiston Course Selection")
+root.geometry("545x385")
 
 root.mainloop()
